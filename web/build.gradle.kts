347ef66c51689f48a14a23c8aa23feac398e613a
Eduardo Osumi <edu_osumi@MacBook-Pro-de-Eduardo.local>
2021-06-22 21:09:08 -0300

    inclusao do swagger

--- web/build.gradle.kts
+++ web/build.gradle.kts
@@ -1,4 +1,8 @@
 dependencies {
     implementation(project(":entities"))
     implementation(project(":repository"))
+
+    implementation("io.springfox:springfox-swagger2:2.9.1")
+    implementation("io.springfox:springfox-swagger-ui:2.9.1")
 }
+
